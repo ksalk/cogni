cb9bd0c9dbd0d89d4c6f690b5b4c7a0b1f0789ad
ksalk <konrad.salkowski+github@gmail.com>
2023-05-27 13:45:09 +0200

    Use array as perceptron's collection and remove unnecessary type

--- src/Cogni/MultilayerPerceptronNetwork/PerceptronLayer.cs
+++ src/Cogni/MultilayerPerceptronNetwork/PerceptronLayer.cs
@@ -3,19 +3,17 @@ namespace Cogni.MultilayerPerceptronNetwork;
 public class PerceptronLayer
 {
     public PerceptronLayerType LayerType { get; set; }
-    public int NumberOfPerceptrons => Perceptrons.Count;
-    public List<Perceptron> Perceptrons { get; set; }
+    public int NumberOfPerceptrons => Perceptrons.Length;
+    public Perceptron[] Perceptrons { get; set; }
 
-    private PerceptronLayer() {
-        Perceptrons = new List<Perceptron>();
-     }
+    private PerceptronLayer() { }
     
     public static PerceptronLayer CreateInputLayer(int numberOfInputs)
     {
         var layer = new PerceptronLayer();
         layer.Perceptrons = Enumerable.Range(0, numberOfInputs)
             .Select(i => new Perceptron(1, PerceptronLayerType.Input, i))
-            .ToList();
+            .ToArray();
 
         layer.LayerType = PerceptronLayerType.Input;
         return layer;
@@ -26,7 +24,7 @@ public class PerceptronLayer
         var layer = new PerceptronLayer();
         layer.Perceptrons = Enumerable.Range(0, numberOfPerceptrons)
             .Select(i => new Perceptron(numberOfPreviousLayerPerceptrons, PerceptronLayerType.Hidden, i))
-            .ToList();
+            .ToArray();
 
         layer.LayerType = PerceptronLayerType.Hidden;
         return layer;
@@ -37,7 +35,7 @@ public class PerceptronLayer
         var layer = new PerceptronLayer();
         layer.Perceptrons = Enumerable.Range(0, numberOfOutputs)
             .Select(i => new Perceptron(numberOfPreviousLayerPerceptrons, PerceptronLayerType.Output, i))
-            .ToList();
+            .ToArray();
 
         layer.LayerType = PerceptronLayerType.Output;
         return layer;
@@ -45,18 +43,18 @@ public class PerceptronLayer
 
     public double[] CalculateOutput(double[] input)
     {
-        var output = new double[Perceptrons.Count];
+        var output = new double[Perceptrons.Length];
 
         if(LayerType == PerceptronLayerType.Input)
         {
-            for (int i = 0; i < Perceptrons.Count; i++)
+            for (int i = 0; i < Perceptrons.Length; i++)
             {
                 output[i] = Perceptrons[i].Predict(new double[] { input[i] });
             }
         }
         else
         {
-            for (int i = 0; i < Perceptrons.Count; i++)
+            for (int i = 0; i < Perceptrons.Length; i++)
             {
                 output[i] = Perceptrons[i].Predict(input);
             }
