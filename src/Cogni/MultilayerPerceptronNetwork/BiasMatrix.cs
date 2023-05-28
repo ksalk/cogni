7dc53be470443c80e9812b831d4ad6e6700efa3c
ksalk <konrad.salkowski+github@gmail.com>
2023-05-28 17:44:30 +0200

    Use arrays to store neurons weights and outputs for better performance

---
+++ src/Cogni/MultilayerPerceptronNetwork/BiasMatrix.cs
@@ -0,0 +1,13 @@
+﻿namespace Cogni.MultilayerPerceptronNetwork
+{
+    internal class BiasMatrix : Matrix
+    {
+        public BiasMatrix(int perceptrons) : base(1, perceptrons)
+        {
+            for (int i = 0; i < perceptrons; i++)
+            {
+                Values[0, i] = Random.Shared.NextDouble() * 2 - 1;
+            }
+        }
+    }
+}
---
+++ src/Cogni/MultilayerPerceptronNetwork/WeightMatrix.cs
@@ -0,0 +1,19 @@
+﻿namespace Cogni.MultilayerPerceptronNetwork
+{
+    internal class WeightMatrix : Matrix
+    {
+        public int InputsCount => Rows;
+        public int OutputsCount => Columns;
+
+        public WeightMatrix(int rows, int cols) : base(rows, cols)
+        {
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    Values[i, j] = Random.Shared.NextDouble() * 2 - 1;
+                }
+            }
+        }
+    }
+}
